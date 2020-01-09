dfac7a0c2f7f7e3373415eb032a362a511767b7f
Alex Pasechnuy <alexpasechnuy@gmail.com>
2020-01-09 23:01:15 +0200

    Lab2 prepared

--- Labs/Lab2/Main.cs
+++ Labs/Lab2/Main.cs
@@ -10,6 +10,32 @@ namespace Labs.Lab2
     {
         public static void Run()
         {
+            Console.WriteLine("1. Individual task");
+            Console.WriteLine("2. Expansion of String Class");
+            Console.WriteLine("3. Expanding Library of Mathematical Functions (Advanced Task)");
+            Console.WriteLine("4. Creation of Class \"Complex\"");
+            Console.WriteLine("5. Creation of Class \"Vector\"");
+            Console.WriteLine("6. Class Hierarchy");
+            Console.WriteLine("7. Roots of an Equation");
+            Console.WriteLine("8. 3D Point\n");
+            Console.WriteLine("Input number of task: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            switch (num)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
