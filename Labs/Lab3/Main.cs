c0c33125c834543d17f4ce3e20dad625f6142ce8
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-01-23 17:56:52 +0200

    Lab3.TextFiles implemented

--- Labs/Lab3/Main.cs
+++ Labs/Lab3/Main.cs
@@ -10,6 +10,45 @@ namespace Labs.Lab3
     {
         public static void Run()
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Individual task");
+                Console.WriteLine("2. Working with Text Files");
+                Console.WriteLine("3. Implementation of Serialization and Deserialization");
+                Console.WriteLine("4. Creation of a Library that Provides Generic Functions for Working with Arrays and Lists");
+                Console.WriteLine("5. Creating Your Own Container");
+                Console.WriteLine("6. Working with Set");
+                Console.WriteLine("7. Working with Associative Array");
+                Console.WriteLine("8. Creating a \"Flexible\" Array (Advanced Task)\n");
+                Console.WriteLine("Input number of task('-1' to return): ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (num)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        TextFiles.Main.Run();
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case -1:
+                        return;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
--- Labs/Lab3/TextFiles.cs
+++ Labs/Lab3/TextFiles.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text;
+using System.IO;
 
-namespace Labs.Lab3
+namespace Labs.Lab3.TextFiles
 {
     class TextFiles
     {
+        public static void Rewrite(String from, String to, int length)
+        {
+            using (StreamReader reader = new StreamReader(from, Encoding.Default))
+            {
+                using (StreamWriter writer = new StreamWriter(to))
+                {
+                    string s;
+                    while ((s = reader.ReadLine()) != null)
+                    {
+                        if(s.Length < length)
+                        {
+                            writer.WriteLine(s);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    class Main
+    {
+        public static void Run()
+        {
+            TextFiles.Rewrite("Files/Lab3/TextFiles/From.txt", "Files/Lab3/TextFiles/To.txt", 7);
+        }
     }
 }
