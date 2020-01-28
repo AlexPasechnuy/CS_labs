4189a9b3ef5c669104701a82ea3296d684d89e4a
AlexPasechnuy <alexpasechnuy@gmail.com>
2020-01-28 22:53:51 +0200

    Lab3.Container: Second variant using list added

--- Labs/Lab3/Container.cs
+++ Labs/Lab3/Container.cs
@@ -74,11 +74,93 @@ namespace Labs.Lab3.Container
         }
     }
 
+    class ListFromIndex<T>
+    {
+        private List<T> arr = new List<T>(0);
+        private int fromIndex;
+
+        public int FromIndex
+        {
+            get { return fromIndex; }
+        }
+
+        public int ToIndex
+        {
+            get { return fromIndex + arr.Count - 1; }
+        }
+
+        public T this[int index]
+        {
+            get { return arr[index - fromIndex]; }
+            set { arr[index - fromIndex] = value; }
+        }
+
+        public int Length
+        {
+            get { return arr.Count; }
+        }
+
+        public ListFromIndex(int from, int to)
+        {
+            fromIndex = from;
+            T[] temp = new T[to - from + 1];
+            arr = new List<T>(temp);
+        }
+
+        public ListFromIndex(int fromIndex, params T[] arr)
+        {
+            this.fromIndex = fromIndex;
+            foreach (T x in arr)
+            {
+                this.arr.Add(x);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T x in arr)
+            {
+                yield return x;
+            }
+        }
+
+        public static implicit operator string(ListFromIndex<T> arr)
+        {
+            string s = "[";
+            foreach (T elem in arr)
+            {
+                s += elem + ", ";
+            }
+            return s.Substring(0, s.Length - 2) + "]";
+        }
+
+        public void Add(T elem)
+        {
+            arr.Add(elem);
+        }
+
+        public void RemoveLast()
+        {
+            if (arr.Count > 0)
+            {
+                arr.RemoveAt(arr.Count - 1);
+            }
+        }
+    }
+
     class Main
     {
         public static void Run()
         {
+            Console.WriteLine("Using arr: ");
+            ArrUsage();
+            Console.WriteLine("\nUsing list: ");
+            ListUsage();
+            Console.ReadKey();
+        }
 
+        private static void ArrUsage()
+        {
             ArrFromIndex<int> intArr = new ArrFromIndex<int>(3, 5);
             Console.WriteLine(intArr);
             intArr[3] = 5;
@@ -99,7 +181,30 @@ namespace Labs.Lab3.Container
             strArr.Add("nine");
             strArr.RemoveLast();
             Console.WriteLine(strArr);
-            Console.ReadKey();
+        }
+
+        private static void ListUsage()
+        {
+            ListFromIndex<int> intArr = new ListFromIndex<int>(3, 5);
+            Console.WriteLine(intArr);
+            intArr[3] = 5;
+            intArr[4] = 7;
+            intArr[5] = 9;
+            intArr.Add(10);
+            intArr.Add(12);
+            intArr.RemoveLast();
+            Console.WriteLine(intArr);
+            foreach (int x in intArr)
+            {
+                Console.WriteLine(x);
+            }
+            string[] temp = { "five", "sixxx", "seven" };
+            ListFromIndex<string> strArr = new ListFromIndex<string>(5, temp);
+            strArr[6] = "six";
+            strArr.Add("eight");
+            strArr.Add("nine");
+            strArr.RemoveLast();
+            Console.WriteLine(strArr);
         }
     }
 }
